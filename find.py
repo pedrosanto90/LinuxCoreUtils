1182d050354f29e35d875c534c649df9bbd391b2
pedrosanto90 <pedro.bb.90@gmail.com>
2024-03-27 14:49:59 +0000

    implementing the function to search the file across all dir in a computer

---
+++ find.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+path = '/home/pedro'
+
+def find_file(filename):
+    for root, dirs, files in os.walk(path):
+        if filename in files:
+            return os.path.join(root)
+    return None
+try:
+    if __name__ == '__main__':
+        filename = sys.argv[1]
+
+        result = find_file(filename)
+
+        if result:
+            print(result)
+
+        else:
+            print(f'{filename} Not Found')
+
+except:
+    print('No Argument')
